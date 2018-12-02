13d313ad2fb8411481412298348590844009c849
Giulio Garofalo <giulio.garofalo832@gmail.com>
2018-12-02 22:10:54 +0100

    vuetify works

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+// main.styl
+// import 'vuetify/src/stylus/main' // Ensure you are using stylus-loader
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
