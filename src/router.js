8793077090a3369a66a3ddb60fc18dcb796c261d
Giulio Garofalo <giulio.garofalo832@gmail.com>
2018-12-08 13:16:35 +0100

    added vue-google-picker - new route /picker + default template

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Picker from './views/Picker.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/picker',
+      name: 'picker',
+      component: Picker
     },
     {
       path: '/about',
---
+++ src/views/Picker.vue
@@ -0,0 +1,40 @@
+<template>
+  <file-picker-button :config="gConfig" @picked="showDetails">
+    Open Google Drive Dialog
+  </file-picker-button>
+</template>
+<script>
+import FilePickerButton from 'vue-google-picker'
+export default {
+  components: {
+    FilePickerButton
+  },
+  mounted() {
+    // addScript: () => {
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://apis.google.com/js/api.js';
+      document.head.appendChild(script);
+    // }
+  },
+  created() {
+    this.gConfig = {
+      // The Browser API key obtained from the Google API Console.
+      developerKey: 'AIzaSyBx8Hc8nc4mC4ojxwIaWyaiE50lFAvr4mM',
+
+      // The Client ID obtained from the Google API Console. Replace with your own Client ID.
+      clientId: '279198720198-aeta0re6qbhavf9f1974dmti3a7b8cp8.apps.googleusercontent.com',
+
+      // Scope to use to access user's drive.
+      scope: 'https://www.googleapis.com/auth/drive.file'
+    }
+  }, 
+  methods: {
+    showDetails (data) {
+     if(data.picked === 'picked') {
+      console.log(data.docs)
+     }
+    }
+  }
+}
+</script>
